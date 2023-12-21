06dddcc2ba0e3239b44c8b4be35932bd6a6c45d8
Jonah <kayiwa.jonah@student.moringaschool.com>
2023-12-21 11:50:32 +0300

    ###functions

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+
+//distance from headquarte in blocks// 
+
+function distanceFromHqInBlocks(SomeValue){
+    const blocks = distanceFromHqInBlocks(SomeValue); 
+    return blocks;
+
+}
+
+//distance from headquarer in ft//
+function distanceFromHqInFeet(someValue){
+    distanceFromHqInBlocks(someValue);
+    const feet = blocks * 264;
+    return feet;
+
+}
+
+//distanceTravelledInFeet//
+function distanceTravelledInFeet(start, destination){
+    const feetPerBlock = 264;
+    const distanceTravelledInFeet = numberOfBlocks * feetPerBlock
+    return distanceTravelledInFeet;
+
+}
+
+//calculatiing the price//
+function calculatesFarePrice(start, destination){
+    
+
+}
