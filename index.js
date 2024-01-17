ff5a797c4177a4c81cc5fe4ca167db382c303ee8
jonah-kayiwa <152336281+jonah-kayiwa@users.noreply.github.com>
2024-01-17 09:18:41 +0300

    completed

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
-// Code your solution in this file!
-
-
-//distance from headquarte in blocks// 
-
-function distanceFromHqInBlocks(SomeValue){
-    const blocks = distanceFromHqInBlocks(SomeValue); 
-    return blocks;
-
+// Function to calculate the distance from Scuber headquarters in blocks
+function distanceFromHqInBlocks(pickupLocation){
+    const hqLocation = 42; //scuber headquarters on 42nd street
+    return Math.abs(pickupLocation - hqLocation);
 }
-
-//distance from headquarer in ft//
-function distanceFromHqInFeet(someValue){
-    distanceFromHqInBlocks(someValue);
-    const feet = blocks * 264;
-    return feet;
-
+function distanceFromHqInFeet (block){
+    const headQuarter=42;
+    const pickupLocation=block;
+    return Math.abs((headQuarter - pickupLocation))*264;
 }
-
-//distanceTravelledInFeet//
 function distanceTravelledInFeet(start, destination){
-    const feetPerBlock = 264;
-    const distanceTravelledInFeet = numberOfBlocks * feetPerBlock
-    return distanceTravelledInFeet;
-
+    const blockLength = 264;
+    return Math.abs(destination - start)*blockLength;
 }
-
-//calculatiing the price//
 function calculatesFarePrice(start, destination){
-    
-
-}
+    const distance = distanceTravelledInFeet(start, destination);
+    const distanceFree = distance <= 400;
+    if (distanceFree){
+        return 0
+    }else if(distance>400 && distance<=2000){
+        return (distance -400)*0.02
+    }else if(distance>2000 && distance<=2500){
+        return 25
+    }else if(distance>2500){
+        return ("cannot travel that far")
+    }
+}    
